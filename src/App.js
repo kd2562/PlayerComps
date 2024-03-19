b93621bbc73ecdb105ea296d2348f5273d247881
Kenneth Dorian <kenneth.dorian@wustl.edu>
2024-03-19 00:05:10 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './Header.js'
+import  { useState } from 'react';
+
+function App() {
+	const [title] = useState('TruMedia Data Viz')
+  return (
+		<>
+			<Header title={title}/>
+			<div style={{display: 'flex', justifyContent: 'flex-start', gap: 4}}>
+	     	<svg width={500} height={500} style={{'background-color': 'white', border: '1px solid black'}}> </svg>
+			 	<svg width={500} height={500} style={{'background-color': 'white', border: '1px solid black'}}> </svg>
+			 </div>
+		</>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header({title}: {title: string}) {
+  return (
+    <h3>
+      {title}
+    </h3>
+  );
+}
+
+export default Header;

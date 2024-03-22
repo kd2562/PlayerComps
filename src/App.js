0ae909b610ddc03d9bf765196d2ce04858257edc
Kenneth Dorian <kenneth.dorian@wustl.edu>
2024-03-21 20:19:15 -0400

    styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import Header from './Header.js'
-import  { useState, useEffect } from 'react';
+import  { useState, useEffect, useRef } from 'react';
+import *  as d3 from 'd3'
 import { apiKey } from './constants.js'
 
+const margins = {left: 5, top: 5, right: 5, bottom: 5}
 
 function App() {
-	const [title] = useState('TruMedia Data Viz Project')
 	const [token, setToken] = useState(null)
 	const [prevToken, setPrevToken] = useState(null)
-	const [gameLogs, setGameLogs] = useState([])
 	const [aggData, setAggData]  = useState([])
+	const [svgWidth, setSVGWidth] = useState(500)
+	const [svgHeight, setSVGHeight] = useState(500)
+
+	const svgRef = useRef(null)
+	const parentElement = svgRef.current?.parentElement
 	const getToken = async () => {
     const resp = await fetch('https://project.trumedianetworks.com/api/token', {
 			method: 'GET',
@@ -33,38 +38,63 @@ function App() {
     setAggData(data);
   }
 
-
-	const getBoxScoreData = async (tempToken: string) => {
-		const resp = await fetch('https://project.trumedianetworks.com/api/mlb/dataviz-data-by-game/1000', {
-			method: 'GET',
-			accept: 'application/json',
-			headers: {
-				tempToken: token
-			}
-		});
-    const data = await resp.json();
-    setGameLogs(data);
-  }
-
 	if(!token){
 		getToken()
 	}
-	console.log(token)
+
 	if(token && !prevToken){
 		setPrevToken(token)
 		getAggData()
-		getBoxScoreData()
 	}
 
-	console.log(gameLogs)
+
+	useEffect(() => {
+		if(parentElement){
+			setSVGWidth(d3.min([parentElement.clientWidth, 500]))
+			setSVGHeight(d3.min([parentElement.clientWidth, 500]))
+		}
+
+		function handleResize(this: Window){
+			if(parentElement){
+				setSVGWidth(d3.min([parentElement.clientWidth, 500]))
+				setSVGHeight(d3.min([parentElement.clientWidth, 500]))
+			}
+		}
+
+		window.addEventListener('resize', handleResize)
+		return () => window.removeEventListener('resize', handleResize)
+
+	}, [parentElement])
+	console.log(svgHeight)
+	useEffect(() => {
+		d3.select(svgRef.current).selectAll('*').remove()
+		const height = svgHeight - margins.top - margins.bottom
+		const width = svgWidth - margins.left - margins.right
+
+		const svg = d3.select(svgRef.current)
+		.attr('width', svgWidth)
+		.attr('height', svgHeight)
+		.append('g')
+		.attr('transform', `translate(${margins.left},${margins.top})`)
+
+		const yScale = d3.scaleLinear().domain([0,100]).range([height, 0])
+		const xScale = d3.scaleLinear().domain([0,100]).range([0, width])
+
+	svg.append('circle')
+		.attr('cx', xScale(50))
+		.attr('cy', yScale(50))
+		.attr('r', 10)
+		.attr('fill', 'cyan')
+		.attr('stroke', 'black')
+
+	}, [aggData, svgWidth, svgHeight])
 
 
   return (
 		<>
-			<Header title={title}/>
-			<div style={{display: 'flex', justifyContent: 'flex-start', gap: 4}}>
-	     	<svg width={500} height={500} style={{'backgroundColor': 'white', border: '1px solid black'}}> </svg>
-			 	<svg width={500} height={500} style={{'backgroundColor': 'white', border: '1px solid black'}}> </svg>
+			<Header title="Player Comps"/>
+			<div style={{display: 'flex', justifyContent: 'flex-start', gap: 4, width: '50%', marginTop: '90px'}}>
+	     	<svg ref={svgRef} style={{ border: '1px solid #597E52', backgroundColor: 'white'}}> </svg>
 			 </div>
 		</>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,16 @@
 function Header({title}: {title: string}) {
   return (
-    <h2>
-      {title}
-    </h2>
+		<div style={{height: '70px',
+			backgroundColor: '#597E52',
+			position: 'absolute',
+			top: '0px', left: '0px',
+			width: '100%',
+			borderBottom: '1px solid gray',
+			boxShadow: `black 0px 0px 10px`}}>
+	    <h2 style={{ paddingLeft: '10px', color: '#F1E4C3', fontSize: '26px'}}>
+	      {title}
+	    </h2>
+		</div>
   );
 }
 
